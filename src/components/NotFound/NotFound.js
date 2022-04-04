bef6309e6ee75dd49efcd196ab61f6b0a7399622
shakhawath <m.shakhawath18@gmail.com>
2022-04-04 22:54:57 +0600

    added not found page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,8 +3,8 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className='text-center mt-14'>
-            <h2 className='text-3xl font-bold'>Not Found</h2>
-            <h3 className='text-2xl'>404</h3>
+            <h2 className='text-6xl font-semibold text-red-700'>404</h2>
+            <h3 className='text-3xl font-bold'>Not Found</h3>
         </div>
     );
 };
